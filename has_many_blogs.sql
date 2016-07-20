48fe54ffece960d98f9c6688776504bbf4df4890
SDodson <siddodson@gmail.com>
2016-07-19 23:15:17 -1000

    Foreign Keys assigned

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -1,18 +1,15 @@
 DROP DATABASE IF EXISTS has_many_blogs;
 DROP user IF EXISTS has_many_user;
-
 CREATE user has_many_user;
 CREATE DATABASE has_many_blogs OWNER has_many_user;
 
 \connect has_many_blogs;
-
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS comments;
 
 CREATE TABLE posts
-(
-  id serial,
+( id serial,
   username character varying(90) NOT NULL,
   first_name character varying(90) DEFAULT NULL,
   last_name character varying(90) DEFAULT NULL,
@@ -21,3 +18,28 @@ CREATE TABLE posts
   PRIMARY KEY (id)
 );
 
+CREATE TABLE posts
+( id serial,
+  title character varying(180) DEFAULT NULL,
+  url character varying(510) DEFAULT NULL,
+  content text DEFAULT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  user_id integer,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE comments
+( id serial,
+  body character varying(510) DEFAULT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  user_id integer,
+  posts_id integer,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (posts_id) REFERENCES posts(id)
+);
+
+\i scripts/blog_data.sql;
