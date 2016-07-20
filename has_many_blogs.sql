e9670d77f2965160a9a3f5e7dd0268c76903c423
SDodson <siddodson@gmail.com>
2016-07-19 22:44:46 -1000

    Create Table

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -2,9 +2,22 @@ DROP DATABASE IF EXISTS has_many_blogs;
 DROP user IF EXISTS has_many_user;
 
 CREATE user has_many_user;
-
 CREATE DATABASE has_many_blogs OWNER has_many_user;
 
 \connect has_many_blogs;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS comments;
+
+CREATE TABLE posts
+(
+  id serial,
+  username character varying(90) NOT NULL,
+  first_name character varying(90) DEFAULT NULL,
+  last_name character varying(90) DEFAULT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP WITH TIME ZONE  NOT NULL DEFAULT now(),
+  PRIMARY KEY (id)
+);
+
