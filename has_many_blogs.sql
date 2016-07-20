c354160884f2a47d8a1f2012afe1a53cbf2c165a
SDodson <siddodson@gmail.com>
2016-07-19 23:15:39 -1000

    Foreign Keys assigned

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -1,13 +1,32 @@
+-- # Has Many Relationships
+
+-- When connecting to your pg database, connect from this project directory.
+
+-- To run sql statements from an external file, use the \i [filepath] command.
+
+-- ### SQL 1-n Relationships and Joins Exercise in PostgreSQL
+
+
+-- ## Setup schema and relationships
+
+
+
+-- 1. Create a new postgres user named `has_many_user`
 DROP DATABASE IF EXISTS has_many_blogs;
+
+-- 1. Create a new database named `has_many_blogs` owned by `has_many_user`
 DROP user IF EXISTS has_many_user;
 CREATE user has_many_user;
 CREATE DATABASE has_many_blogs OWNER has_many_user;
-
 \connect has_many_blogs;
+
+-- 1. Before each create table statement, add a drop table if exists statement.
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS comments;
 
+-- 1. In `has_many_blogs.sql` Create the tables (including any PKs, Indexes, and Constraints that you may need) to fulfill the requirements of the **has_many_blogs schema** below.
+-- 1. Create the necessary FKs needed to relate the tables according to the **relationship table** below.
 CREATE TABLE posts
 ( id serial,
   username character varying(90) NOT NULL,
@@ -41,5 +60,5 @@ CREATE TABLE comments
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (posts_id) REFERENCES posts(id)
 );
-
+-- 1. Run the provided `scripts/blog_data.sql`
 \i scripts/blog_data.sql;
